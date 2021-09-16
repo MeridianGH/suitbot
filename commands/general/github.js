46538a2c84a07941d3d469de4d5b6dd4440f2321
MeridianPY <lukas.romacker@t-online.de>
2021-09-16 23:04:15 +0200

    Added help and github

---
+++ commands/general/github.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { simpleEmbed } = require('../../utilities');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('github')
+        .setDescription('Sends a link to the repo of this bot.'),
+    async execute(interaction) {
+        await interaction.reply(simpleEmbed('[GitHub](https://github.com/MeridianPY/suitbot)'));
+    },
+};
