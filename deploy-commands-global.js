5a7394328890f7f97a366c8d2f02992b772d0e9d
MeridianPY <lukas.romacker@t-online.de>
2021-09-16 23:04:53 +0200

    Refactoring and removing dependencies

--- deploy-commands-global.js
+++ deploy-commands-global.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./config.json');
+const { appId, token } = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -30,7 +30,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 (async () => {
     try {
         await rest.put(
-            Routes.applicationCommands(clientId),
+            Routes.applicationCommands(appId),
             { body: commands },
         );
 
--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const { appId, guildId, token } = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -30,7 +30,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 (async () => {
     try {
         await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
+            Routes.applicationGuildCommands(appId, guildId),
             { body: commands },
         );
 
