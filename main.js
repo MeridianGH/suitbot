1f709f0c4e7ab6288ab0940a56ee73e52e423814
MeridianPY <lukas.romacker@t-online.de>
2021-09-17 20:31:50 +0200

    Fix token import

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const { Player } = require('discord-music-player')
 // Check if running in Heroku
 let token = process.env.token
 if (!token) {
-  const { configToken } = require('./config.json')
-  token = configToken
+  const config = require('./config.json')
+  token = config.token
 }
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES] })
