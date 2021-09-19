1fed7777dd15133853e9a032e7aad22cdfc7efb0
MeridianPY <lukas.romacker@t-online.de>
2021-09-19 15:01:00 +0200

    Added activity command

---
+++ commands/general/activity.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { simpleEmbed } = require('../../utilities')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('activity')
+    .setDescription('Creates a Discord activity.')
+    .addStringOption(option => option.setName('activity').setDescription('The activity to create').setRequired(true)
+      .addChoice('YouTube Together', '755600276941176913')
+      .addChoice('Poker Night', '755827207812677713')
+      .addChoice('Betrayal.io', '773336526917861400')
+      .addChoice('Fishington.io', '814288819477020702')
+      .addChoice('Chess in the Park', '832012774040141894'))
+    .addChannelOption(option => option.setName('channel').setDescription('The voice channel to create the activity in.').setRequired(true)),
+  async execute (interaction) {
+    const channel = interaction.options.getChannel('channel')
+    if (!channel.isVoice()) {
+      return await interaction.reply(simpleEmbed('You can only specify a voice channel!', true))
+    }
+
+    const rest = new REST({ version: '8' }).setToken(interaction.client.token)
+
+    await (async () => {
+      try {
+        await rest.post(
+          Routes.channelInvites(channel.id),
+          {
+            body: {
+              target_application_id: interaction.options.getString('activity'),
+              target_type: 2
+            }
+          }
+        ).then(response => interaction.reply(simpleEmbed(`[Click here to open Activity](https://discord.gg/${response.code})`)))
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }
+}
