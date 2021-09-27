9a5858891803dd5b6188011649c045581b15c707
MeridianPY <lukas.romacker@t-online.de>
2021-09-27 19:02:30 +0200

    Update error.js

--- events/player/error.js
+++ events/player/error.js
@@ -2,6 +2,6 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
   name: 'error',
   execute (error, queue) {
-    queue.lastTextChannel.send({ embeds: [new MessageEmbed().setDescription(error).setFooter('SuitBot', require('../client/ready').iconURL).setColor('#ff0000')] })
+    if (queue.lastTextChannel) { queue.lastTextChannel.send({ embeds: [new MessageEmbed().setTitle('Error').setDescription(error).setFooter('SuitBot', require('../client/ready').iconURL).setColor('#ff0000')] }) }
   }
 }
