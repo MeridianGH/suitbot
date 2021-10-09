aee765f118f4fa8bae1eb663b2aef145b4122335
MeridianPY <lukas.romacker@t-online.de>
2021-10-09 03:33:06 +0200

    Simplify seek

--- commands/2. Music/seek.js
+++ commands/2. Music/seek.js
@@ -9,14 +9,9 @@ module.exports = {
     .addStringOption(option => option.setName('time').setDescription('The time to skip to. Can be seconds or HH:MM:SS.').setRequired(true)),
   async execute (interaction) {
     const time = interaction.options.getString('time')
+    const milliseconds = isNaN(time) ? Utils.timeToMs(time) : time * 1000
     const queue = interaction.client.player.getQueue(interaction.guild.id)
     if (!queue || !queue.nowPlaying) { return await interaction.reply(simpleEmbed('Nothing currently playing.\nStart playback with /play!', true)) }
-    let milliseconds
-    if (isNaN(time)) {
-      milliseconds = Utils.timeToMs(time)
-    } else {
-      milliseconds = time * 1000
-    }
     if (milliseconds < 0 || milliseconds > queue.nowPlaying.millisecons) { return interaction.reply(simpleEmbed(`You can only seek between 0:00-${queue.nowPlaying.duration}!`, true)) }
     await queue.seek(milliseconds)
     await interaction.reply(simpleEmbed(`⏩ Skipped to ${msToHMS(milliseconds)}.`))
