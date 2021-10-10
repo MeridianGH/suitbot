a560d0214dc9339ca93c83b70674a599aa717ce6
MeridianPY <lukas.romacker@t-online.de>
2021-10-10 03:52:29 +0200

    Update error.js

--- events/player/error.js
+++ events/player/error.js
@@ -2,6 +2,7 @@ const { errorEmbed } = require('../../utilities')
 module.exports = {
   name: 'error',
   execute (error, queue) {
+    if (error === 'Status code: 410') { return queue.lastTextChannel?.send(errorEmbed('Error', 'Status code: 410\nIs the video possibly age restricted?\nAge restricted videos currently don\'t work!')) }
     queue.lastTextChannel?.send(errorEmbed('Error', error))
   }
 }
