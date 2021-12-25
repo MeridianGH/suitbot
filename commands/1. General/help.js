a710008a25294b24225b8d6580ef7b2c99b4f567
MeridianPY <lukas.romacker@t-online.de>
2021-12-25 21:29:01 +0100

    Update help command

--- commands/1. General/help.js
+++ commands/1. General/help.js
@@ -27,11 +27,12 @@ module.exports = {
     const embed = new MessageEmbed()
       .setAuthor('Help', interaction.member.user.displayAvatarURL())
       .setTitle('SuitBot Help Page')
+      .setThumbnail(interaction.client.user.displayAvatarURL())
       .setDescription('This module lists every command SuitBot currently supports.\n\nTo use a command start by typing `/` followed by the command you want to execute. You can also use Discord\'s integrated auto-completion for commands.\n\n')
       .addField('➕  Invite', '[Click here to invite](https://discord.com/oauth2/authorize?client_id=887122733010411611&scope=bot%20applications.commands&permissions=2167425024)', true)
       .addField('🌐  Website', '[suitbot.xyz](https://suitbot.xyz)', true)
       .addField('<:github:923336812410306630>  Source code', '[GitHub](https://github.com/MeridianGH/suitbot)', true)
-      .addField('\u200b', 'Press the buttons below to switch between pages and display more info.')
+      .addField('\u200b', 'Press the buttons below to switch pages and display more info.')
       .setFooter(`SuitBot | Page ${pages.length + 1}/${Object.entries(categories).length + 1}`, interaction.client.user.displayAvatarURL())
     pages.push(embed)
 
