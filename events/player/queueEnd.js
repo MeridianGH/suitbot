48ffd47bc178c1be0685d235a67ba6fce0218832
MeridianPY <lukas.romacker@t-online.de>
2022-01-03 21:21:01 +0100

    Remove debug log

--- events/player/queueEnd.js
+++ events/player/queueEnd.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'queueEnd',
   execute (queue) {
     setTimeout(() => {
-      if (!queue.isPlaying) { queue.stop() } else { console.log('cancelled') }
+      if (!queue.isPlaying) { queue.stop() }
     }, 30000)
   }
 }
