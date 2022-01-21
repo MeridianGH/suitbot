ce24bc4791f7391d276f4b7999e593151e5b99f7
MeridianPY <lukas.romacker@t-online.de>
2022-01-21 20:36:37 +0100

    Add file and line to error message

--- events/process/uncaughtException.js
+++ events/process/uncaughtException.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'uncaughtException',
   execute (error) {
-    console.log('Ignoring uncaught exception: ' + error)
+    console.log(`Ignoring uncaught exception: ${error} | ${error.stack.split(/\r?\n/)[1].split('\\').pop().slice(0, -1)}`)
   }
 }
