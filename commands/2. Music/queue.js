6d60aeb1dcc435f0370dfe6babcc4fb7c8419c02
MeridianPY <lukas.romacker@t-online.de>
2022-03-15 13:33:57 +0100

    Simplify queue.js

--- commands/2. Music/queue.js
+++ commands/2. Music/queue.js
@@ -15,22 +15,20 @@ module.exports = {
 
     if (queue.tracks.length === 0) {
       // Format single page with no upcoming songs.
-      let description = ''
-      description += 'Still using old and boring commands? Use the new [web dashboard](https://suitbot.xyz) instead!\n\n'
+      let description = 'Still using old and boring commands? Use the new [web dashboard](https://suitbot.xyz) instead!\n\n'
       description += `Now Playing:\n[${queue.current.title}](${queue.current.url}) | \`${queue.current.durationMS === 0 ? '🔴 Live' : queue.current.duration}\`\n\n`
+      description += `No upcoming songs.\nAdd songs with /play!\n${'\u2015'.repeat(34)}`
+
       const embed = new MessageEmbed()
         .setAuthor({ name: 'Queue.', iconURL: interaction.member.user.displayAvatarURL() })
-        .setDescription(description + `No upcoming songs.\nAdd songs with /play!\n${'\u2015'.repeat(34)}`)
+        .setDescription(description)
         .setFooter({ text: `SuitBot | Page 1/1 | Repeat: ${{ 0: '❌', 1: '🔂 Track', 2: '🔁 Queue', 3: '⏩ Autoplay' }[queue.repeatMode]}`, iconURL: interaction.client.user.displayAvatarURL() })
       pages.push(embed)
     } else if (queue.tracks.length >= 1 && queue.tracks.length <= 10) {
       // Format single page.
-      let description = ''
-      description += 'Still using old and boring commands? Use the new [web dashboard](https://suitbot.xyz) instead!\n\n'
-      description += `Now Playing:\n[${queue.current.title}](${queue.current.url}) | \`${queue.current.durationMS === 0 ? '🔴 Live' : queue.current.duration}\`\n\nUp Next:\n`
-      for (const track of queue.tracks) {
-        description += `\`${(queue.getTrackPosition(track) + 1)}.\` [${track.title}](${track.url}) | \`${track.durationMS === 0 ? '🔴 Live' : track.duration}\`\n\n`
-      }
+      let description = 'Still using old and boring commands? Use the new [web dashboard](https://suitbot.xyz) instead!\n\n'
+      description += `Now Playing:\n[${queue.current.title}](${queue.current.url}) | \`${queue.current.durationMS === 0 ? '🔴 Live' : queue.current.duration}\`\n\n`
+      for (const track of queue.tracks) { description += `\`${(queue.getTrackPosition(track) + 1)}.\` [${track.title}](${track.url}) | \`${track.durationMS === 0 ? '🔴 Live' : track.duration}\`\n\n` }
       description += `**${queue.tracks.length} songs in queue | ${msToHMS(queue.totalTime)} total duration**\n${'\u2015'.repeat(34)}`
 
       const embed = new MessageEmbed()
@@ -43,12 +41,9 @@ module.exports = {
       for (let i = 0; i < queue.tracks.length; i += 10) {
         const tracks = queue.tracks.slice(i, i + 10)
 
-        let description = ''
-        description += 'Still using old and boring commands? Use the new [web dashboard](https://suitbot.xyz) instead!\n\n'
-        description += `Now Playing:\n[${queue.current.title}](${queue.current.url}) | \`${queue.current.durationMS === 0 ? '🔴 Live' : queue.current.duration}\`\n\nUp Next:\n`
-        for (const track of tracks) {
-          description += `\`${(queue.getTrackPosition(track) + 1)}.\` [${track.title}](${track.url}) | \`${track.durationMS === 0 ? '🔴 Live' : track.duration}\`\n\n`
-        }
+        let description = 'Still using old and boring commands? Use the new [web dashboard](https://suitbot.xyz) instead!\n\n'
+        description += `Now Playing:\n[${queue.current.title}](${queue.current.url}) | \`${queue.current.durationMS === 0 ? '🔴 Live' : queue.current.duration}\`\n\n`
+        for (const track of tracks) { description += `\`${(queue.getTrackPosition(track) + 1)}.\` [${track.title}](${track.url}) | \`${track.durationMS === 0 ? '🔴 Live' : track.duration}\`\n\n` }
         description += `**${queue.tracks.length} songs in queue | ${msToHMS(queue.totalTime)} total duration**\n${'\u2015'.repeat(34)}`
 
         const embed = new MessageEmbed()
