a04d36d06156d4f1015cf23b7451d5eb4fe91566
MeridianPY <lukas.romacker@t-online.de>
2022-03-17 02:35:21 +0100

    Use standard

--- dashboard/assets/react/react.js
+++ dashboard/assets/react/react.js
@@ -8,7 +8,6 @@ function send (data) {
 
 const loader = document.getElementById('loader')
 
-
 'use strict'
 
 class App extends React.Component {
@@ -16,6 +15,7 @@ class App extends React.Component {
     super(props)
     this.state = null
   }
+
   componentDidMount () {
     websocket.addEventListener('open', () => {
       console.log('Websocket connected, requesting queue data...')
@@ -40,21 +40,23 @@ class App extends React.Component {
       }
     }, 1000)
   }
+
   componentWillUnmount () {
     clearInterval(this.interval)
   }
+
   render () {
     if (!this.state) { return null }
-    if (this.state.empty) { return <div>Nothing currently playing!<br/>Join a voice channel and type "/play" to get started!</div> }
+    if (this.state.empty) { return <div>Nothing currently playing!<br />Join a voice channel and type "/play" to get started!</div> }
     return (
       <div>
-        <MediaSession track={this.state.current} paused={this.state.paused}/>
+        <MediaSession track={this.state.current} paused={this.state.paused} />
         <h1 className='queue-title'>Now Playing</h1>
-        <NowPlaying track={this.state.current} paused={this.state.paused} streamTime={this.state.streamTime} repeatMode={this.state.repeatMode}/>
-        <VolumeControl volume={this.state.volume}/>
-        <div style={{ marginBottom: 20 }}/>
+        <NowPlaying track={this.state.current} paused={this.state.paused} streamTime={this.state.streamTime} repeatMode={this.state.repeatMode} />
+        <VolumeControl volume={this.state.volume} />
+        <div style={{ marginBottom: 20 }} />
         <h1 className='queue-title'>Queue</h1>
-        <Queue tracks={this.state.tracks}/>
+        <Queue tracks={this.state.tracks} />
       </div>
     )
   }
@@ -73,16 +75,16 @@ function NowPlaying (props) {
     <div className='nowplaying-container'>
       <ul className='horizontal-list'>
         <li>
-          <Thumbnail image={props.track.thumbnail}/>
+          <Thumbnail image={props.track.thumbnail} />
           <div className='progress-container'>
-            <div className='progress' style={{ width: props.track.durationMS === 0 ? '100%' : props.streamTime / props.track.durationMS * 100 + '%' }}/>
+            <div className='progress' style={{ width: props.track.durationMS === 0 ? '100%' : props.streamTime / props.track.durationMS * 100 + '%' }} />
           </div>
         </li>
         <li>
           <a href={props.track.url} target='_blank'><h4>{props.track.title}</h4></a>
           <h6>{props.track.author}</h6>
           <h5>{props.track.durationMS === 0 ? '🔴 Live' : `${msToHMS(props.streamTime)} / ${props.track.duration}`}</h5>
-          <MusicControls paused={props.paused} repeatMode={props.repeatMode}/>
+          <MusicControls paused={props.paused} repeatMode={props.repeatMode} />
         </li>
       </ul>
     </div>
@@ -92,8 +94,8 @@ function NowPlaying (props) {
 function Thumbnail (props) {
   return (
     <div className='thumbnail-container'>
-      <img className='thumbnail-backdrop' src={props.image}/>
-      <img className='thumbnail' src={props.image}/>
+      <img className='thumbnail-backdrop' src={props.image} />
+      <img className='thumbnail' src={props.image} />
     </div>
   )
 }
@@ -104,12 +106,12 @@ function MusicControls (props) {
   }
   return (
     <div>
-      <button className='button square' onClick={onClick.bind(this, 'previous')}><i className='fas fa-backward'/></button>
-      <button className='button square' onClick={onClick.bind(this, 'pause')}><i className={props.paused ? 'fas fa-play' : 'fas fa-pause'}/></button>
-      <button className='button square' onClick={onClick.bind(this, 'skip')}><i className='fas fa-forward'/></button>
+      <button className='button square' onClick={onClick.bind(this, 'previous')}><i className='fas fa-backward' /></button>
+      <button className='button square' onClick={onClick.bind(this, 'pause')}><i className={props.paused ? 'fas fa-play' : 'fas fa-pause'} /></button>
+      <button className='button square' onClick={onClick.bind(this, 'skip')}><i className='fas fa-forward' /></button>
       &nbsp;&nbsp;
-      <button className='button square' onClick={onClick.bind(this, 'shuffle')}><i className='fas fa-random'/></button>
-      <button className='button square' onClick={onClick.bind(this, 'repeat')}><i className={props.repeatMode === 0 ? 'fad fa-repeat-alt' : props.repeatMode === 1 ? 'fas fa-repeat-1-alt' : 'fas fa-repeat'}/></button>
+      <button className='button square' onClick={onClick.bind(this, 'shuffle')}><i className='fas fa-random' /></button>
+      <button className='button square' onClick={onClick.bind(this, 'repeat')}><i className={props.repeatMode === 0 ? 'fad fa-repeat-alt' : props.repeatMode === 1 ? 'fas fa-repeat-1-alt' : 'fas fa-repeat'} /></button>
     </div>
   )
 }
@@ -119,17 +121,20 @@ class VolumeControl extends React.Component {
     super(props)
     this.state = { volume: props.volume }
   }
-  changeVolume = (event) => {
+
+  changeVolume (event) {
     this.setState({ volume: event.target.value })
   }
-  setVolume = (event) => {
+
+  setVolume (event) {
     send({ type: 'volume', volume: event.target.value })
   }
+
   render () {
     return (
       <div>
-        <button className='volume-display' disabled={true}><i className={`fas fa-volume${this.state.volume === 0 ? '-off' : this.state.volume <= 33 ? '-down' : this.state.volume <= 66 ? '' : '-up'}`}/> {this.state.volume}</button>
-        <input className='volume-slider' type="range" defaultValue={this.state.volume} step="10" min="0" max="100" onInput={this.changeVolume} onMouseUp={this.setVolume}/>
+        <button className='volume-display' disabled><i className={`fas fa-volume${this.state.volume === 0 ? '-off' : this.state.volume <= 33 ? '-down' : this.state.volume <= 66 ? '' : '-up'}`} /> {this.state.volume}</button>
+        <input className='volume-slider' type='range' defaultValue={this.state.volume} step='10' min='0' max='100' onInput={this.changeVolume.bind(this)} onMouseUp={this.setVolume.bind(this)} />
       </div>
     )
   }
@@ -146,12 +151,12 @@ function QueueButtons () {
     send({ type: 'clear' })
   }
   return (
-    <div className="queue-button-container">
+    <div className='queue-button-container'>
       <form onSubmit={handlePlay}>
-        <input type="text" className="textfield" placeholder="Add to queue" ref={input}/>
-        <button className="button"><i className="fas fa-plus"/> Play</button>
+        <input type='text' className='textfield' placeholder='Add to queue' ref={input} />
+        <button className='button'><i className='fas fa-plus' /> Play</button>
       </form>
-      <button className="button" onClick={handleClear}><i className="fas fa-trash-alt"/> Clear queue</button>
+      <button className='button' onClick={handleClear}><i className='fas fa-trash-alt' /> Clear queue</button>
     </div>
   )
 }
@@ -164,30 +169,30 @@ function Queue (props) {
   for (let i = 0; i < props.tracks.length; i++) {
     rows.push(
       <tr key={i}>
-        <td><span className="text-nowrap">{i + 1}</span></td>
-        <td><span className="text-nowrap">{props.tracks[i].title}</span></td>
-        <td><span className="text-nowrap">{props.tracks[i].author}</span></td>
-        <td><span className="text-nowrap">{props.tracks[i].durationMS === 0 ? '🔴 Live' : props.tracks[i].duration}</span></td>
-        <td><span className="text-nowrap"><button className="button square transparent" onClick={onClick.bind(this, 'remove', i)}><i className="fas fa-trash-alt"/></button><button className="button square transparent" onClick={onClick.bind(this, 'skipto', i)}><i className="fas fa-forward"/></button></span></td>
+        <td><span className='text-nowrap'>{i + 1}</span></td>
+        <td><span className='text-nowrap'>{props.tracks[i].title}</span></td>
+        <td><span className='text-nowrap'>{props.tracks[i].author}</span></td>
+        <td><span className='text-nowrap'>{props.tracks[i].durationMS === 0 ? '🔴 Live' : props.tracks[i].duration}</span></td>
+        <td><span className='text-nowrap'><button className='button square transparent' onClick={onClick.bind(this, 'remove', i)}><i className='fas fa-trash-alt' /></button><button className='button square transparent' onClick={onClick.bind(this, 'skipto', i)}><i className='fas fa-forward' /></button></span></td>
       </tr>
     )
   }
   return (
     <div>
-      <QueueButtons/>
-      <div className="table-responsive">
-        <table className="table table-dark table-striped">
+      <QueueButtons />
+      <div className='table-responsive'>
+        <table className='table table-dark table-striped'>
           <thead>
-          <tr>
-            <th>#</th>
-            <th style={{ width: '100%' }}>Track</th>
-            <th>Author</th>
-            <th>Duration</th>
-            <th>Actions</th>
-          </tr>
+            <tr>
+              <th>#</th>
+              <th style={{ width: '100%' }}>Track</th>
+              <th>Author</th>
+              <th>Duration</th>
+              <th>Actions</th>
+            </tr>
           </thead>
           <tbody>
-          {rows}
+            {rows}
           </tbody>
         </table>
       </div>
@@ -226,4 +231,4 @@ function MediaSession (props) {
 }
 
 const domContainer = document.querySelector('#react-container')
-ReactDOM.render(<App/>, domContainer)
+ReactDOM.render(<App />, domContainer)
