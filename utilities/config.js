8f483c48b0430a52c770a10bd27b6cca1d8d8257
MeridianPY <lukas.romacker@t-online.de>
2022-04-08 16:59:08 +0200

    Set empty config

--- utilities/config.js
+++ utilities/config.js
@@ -1,7 +1,15 @@
 import { existsSync } from 'fs'
 import { readFile } from 'fs/promises'
 
-const config = existsSync('../config.json') ? JSON.parse(await readFile(new URL('../config.json', import.meta.url))) : {}
+const config = existsSync('../config.json') ? JSON.parse(await readFile(new URL('../config.json', import.meta.url))) : {
+  token: null,
+  appId: null,
+  clientSecret: null,
+  guildId: null,
+  adminId: null,
+  cookie: null,
+  geniusAppId: null
+}
 
 if (process.env.token) {
   Object.keys(config).map(key => {
