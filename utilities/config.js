c68362724db6a3f529fed9f45aa57a2a5e1ea0c0
Meridian <lukas.romacker@t-online.de>
2022-04-16 20:43:30 +0200

    Add config template

--- utilities/config.js
+++ utilities/config.js
@@ -2,13 +2,20 @@ import { existsSync } from 'fs'
 import { readFile } from 'fs/promises'
 
 const file = new URL('../config.json', import.meta.url)
-const config = existsSync(file) ? JSON.parse(await readFile(file)) : {}
+const config = existsSync(file) ? JSON.parse(await readFile(file)) : {
+  token: null,
+  appId: null,
+  clientSecret: null,
+  guildId: null,
+  adminId: null,
+  cookie: null,
+  geniusAppId: null
+}
 
 if (process.env.token) {
   Object.keys(config).map(key => {
     config[key] = process.env[key]
   })
 }
-console.log(config)
 
 export const { token, appId, clientSecret, guildId, adminId, cookie, geniusAppId } = config
