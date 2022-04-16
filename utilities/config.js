19f25bdb0b9b8bfaaaec5a38ff328b8230553f1e
Meridian <lukas.romacker@t-online.de>
2022-04-16 21:22:15 +0200

    Simplify config template

--- utilities/config.js
+++ utilities/config.js
@@ -2,18 +2,10 @@ import { existsSync } from 'fs'
 import { readFile } from 'fs/promises'
 
 const file = new URL('../config.json', import.meta.url)
-const config = existsSync(file) ? JSON.parse(await readFile(file)) : {
-  token: null,
-  appId: null,
-  clientSecret: null,
-  guildId: null,
-  adminId: null,
-  cookie: null,
-  geniusAppId: null
-}
+const config = existsSync(file) ? JSON.parse(await readFile(file)) : {}
 
 if (process.env.token) {
-  Object.keys(config).map(key => {
+  Object.keys(JSON.parse(await readFile(new URL('../config_example.json', import.meta.url)))).map(key => {
     config[key] = process.env[key]
   })
 }
