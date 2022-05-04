2b478019e01d2b1ce3ef53f93d56d7be40893f1e
MeridianPY <lukas.romacker@t-online.de>
2022-05-04 11:51:32 +0200

    Fix and add queries

--- utilities/database.js
+++ utilities/database.js
@@ -4,11 +4,17 @@ const sql = postgres(process.env.DATABASE_URL ?? 'postgres://postgres:postgres@l
 
 export default {
   sql: sql,
+  getLocale: async function getLocale(guildId) {
+    return sql`select locale from servers where id = ${guildId};`
+  },
+  setLocale: async function setLocale(guildId, locale) {
+    await sql`update servers set ${sql({ locale: locale })} where id = ${guildId};`
+  },
   getSetting: async function getSetting(guildId, setting) {
     return sql`select ${setting} from servers where id = ${guildId};`
   },
   setSetting: async function setSetting(guildId, setting, value) {
-    await sql`update servers set ${setting} = ${value} where id = ${guildId};`
+    await sql`update servers set ${sql({ [setting]: value })} where id = ${guildId};`
   },
   getSettings: async function getSettings(guildId) {
     const settings = (await sql`select * from servers where id = ${guildId};`)[0]
