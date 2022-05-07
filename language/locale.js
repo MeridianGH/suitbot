6cab68197c4be050eb3095b11e4102bf266e9e3f
MeridianPY <lukas.romacker@t-online.de>
2022-05-07 13:43:17 +0200

    Use Discord locale strings

--- language/locale.js
+++ language/locale.js
@@ -1,14 +1,14 @@
-import * as deDE from './lang/de-DE.js'
+import * as de from './lang/de.js'
 import * as enUS from './lang/en-US.js'
-import * as fiFI from './lang/fi-FI.js'
+import * as fi from './lang/fi.js'
 import * as ptBR from './lang/pt-BR.js'
 
 // https://discord.com/developers/docs/reference#locales
 
 export default {
-  'de-DE': deDE,
+  'de': de,
   'en-US': enUS,
-  'fi-FI': fiFI,
+  'fi': fi,
   'pt-BR': ptBR,
   'undefined': enUS
 }
