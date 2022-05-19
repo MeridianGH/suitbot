b7df96f565ba4c1bd3f1f25f8137f86728df15de
MeridianPY <lukas.romacker@t-online.de>
2022-05-20 00:19:22 +0200

    Update database.js

--- utilities/database.js
+++ utilities/database.js
@@ -1,12 +1,11 @@
 import postgres from 'postgres'
 
 const sql = process.env.DATABASE_URL ? postgres(process.env.DATABASE_URL, { ssl: { rejectUnauthorized: false } }) : postgres('postgres://postgres:postgres@localhost:5432/suitbot')
-console.log(sql.options.host, sql.options.database)
 
 export default {
   sql: sql,
   getLocale: async function getLocale(guildId) {
-    return (await sql`select locale from servers where id = ${guildId};`)[0].locale
+    return (await sql`select locale from servers where id = ${guildId};`)[0]?.locale ?? 'en-US'
   },
   setLocale: async function setLocale(guildId, locale) {
     await sql`update servers set ${sql({ locale: locale })} where id = ${guildId};`
@@ -16,6 +15,7 @@ export default {
   },
   addAllServers: function addAllServers(guilds) {
     guilds = guilds.map((guild) => ({ id: guild.id, locale: guild.preferredLocale }))
+    console.log(guilds)
     sql`insert into servers ${sql(guilds)} on conflict (id) do nothing;`
   },
 }
