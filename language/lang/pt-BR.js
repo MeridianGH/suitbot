0f5e0cd583cabcd53124d689ff10c4555d6ddd5f
MeridianPY <lukas.romacker@t-online.de>
2022-05-25 12:58:01 +0200

    Update pt-BR.js

--- language/lang/pt-BR.js
+++ language/lang/pt-BR.js
@@ -113,7 +113,7 @@ export const clear = {
   }
 }
 export const filter = {
-  other: { response: (filter) => `Set filter to ${filter}.` },
+  other: { response: (filter) => `Definir filtro para ${filter}.` },
   errors: {
     nothingPlaying: musicErrors.nothingPlaying,
     sameChannel: musicErrors.sameChannel
