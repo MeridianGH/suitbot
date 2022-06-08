b940522883ad86867721ce0e967d6e27895848fa
MeridianPY <lukas.romacker@t-online.de>
2022-06-08 21:11:53 +0200

    Fix nowPlaying check

--- commands/2. Music/clear.js
+++ commands/2. Music/clear.js
@@ -9,7 +9,7 @@ export const { data, execute } = {
   async execute(interaction) {
     const lang = getLanguage(await interaction.client.database.getLocale(interaction.guildId)).clear
     const player = interaction.client.lavalink.getPlayer(interaction.guild.id)
-    if (!player || !player.current) { return await interaction.reply(errorEmbed(lang.errors.nothingPlaying, true)) }
+    if (!player || !player.queue.current) { return await interaction.reply(errorEmbed(lang.errors.nothingPlaying, true)) }
     if (interaction.member.voice.channel.id !== player.voiceChannel) { return await interaction.reply(errorEmbed(lang.errors.sameChannel, true)) }
 
     player.queue.clear()
--- commands/2. Music/seek.js
+++ commands/2. Music/seek.js
@@ -16,7 +16,7 @@ export const { data, execute } = {
     const lang = getLanguage(await interaction.client.database.getLocale(interaction.guildId)).seek
     const time = timeToMs(interaction.options.getString('time'))
     const player = interaction.client.lavalink.getPlayer(interaction.guild.id)
-    if (!player || !player.current) { return await interaction.reply(errorEmbed(lang.errors.nothingPlaying, true)) }
+    if (!player || !player.queue.current) { return await interaction.reply(errorEmbed(lang.errors.nothingPlaying, true)) }
     if (interaction.member.voice.channel.id !== player.voiceChannel) { return await interaction.reply(errorEmbed(lang.errors.sameChannel, true)) }
     if (player.queue.current.isStream) { return await interaction.reply(errorEmbed(lang.errors.isLive, true)) }
     if (time < 0 || time > player.queue.current.duration) { return await interaction.reply(errorEmbed(lang.errors.index(player.queue.current.duration), true)) }
